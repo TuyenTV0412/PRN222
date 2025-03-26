f8535bad9644301b0476384afea1211ac8f80222
TuyenTV0412 <Sipiukun@gmail.com>
2025-03-26 11:22:27 +0700

    ver11

--- PRN222/obj/Debug/net8.0/PRN222.AssemblyInfo.cs
+++ PRN222/obj/Debug/net8.0/PRN222.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN222")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51dbae38928a2a2f3bd208951bf9f0ee1b7d3b22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ab72f130bfaa26a9006472f1cb0504c972a2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN222")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN222")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
